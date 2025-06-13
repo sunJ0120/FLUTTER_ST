728c6c78b4b4efcb7c71a7680cc539dc46ea3342
sunJ0120 <sspure1214@gmail.com>
2025-06-13 23:49:50 +0900

    Study: chap03 처음 만나는 다트 : _ 밑줄 까지

---
+++ lib/ch3/test1.dart
@@ -0,0 +1,27 @@
+int no1 = 10;
+int _no2 = 20;
+
+void sayHello1(){}
+void _sayHello2(){}
+
+class User1{
+  String? name;
+  String _address = 'seoul';
+}
+
+class _User2{}
+
+main(){
+  no1 = 20;
+  _no2 = 30;
+
+  sayHello1();
+  _sayHello2();
+
+  User1 user1 = User1();
+  _User2 user2 = _User2();
+
+  user1.name = 'kkang';
+  user1._address = 'busan';
+}
+
